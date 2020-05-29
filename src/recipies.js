df9ee689b451f1d7345bc3989461faefe6c5e058
GR8 <gr8_boi52@yahoo.com>
2020-05-30 00:01:11 +0400

    Cleanup unneeded files

---
+++ src/recipies.js
@@ -0,0 +1,36 @@
+const recipies = [
+  {
+    handle: 'chicken-curry',
+    author: 'Jim',
+    name: 'Chicken Curry',
+    description: 'Delicious spicy chicken curry',
+    times: {
+      prep: 60,
+      cook: 40
+    },
+    ingredients: ['1lb Chicken', '1/2 tbsp chilli powder'],
+    instructions: [
+      'Preheat oven to 500 F degrees.Spray a baking sheet (15.25 x 10.25 inches) with cooking spray.',
+      'Roll out the pizza dough and place it on the pizza baking dish.',
+      'Garnish with chives and parsley.'
+    ]
+  },
+  {
+    handle: 'hamburger',
+    author: 'Aravind',
+    name: 'Hamburger',
+    description: 'Juicy burger with toppings and a soft bun',
+    times: {
+      prep: 60,
+      cook: 40
+    },
+    ingredients: ['1lb Chicken', '1/2 tbsp chilli powder'],
+    instructions: [
+      'Preheat oven to 500 F degrees.Spray a baking sheet (15.25 x 10.25 inches) with cooking spray.',
+      'Roll out the pizza dough and place it on the pizza baking dish.',
+      'Garnish with chives and parsley.'
+    ]
+  }
+]
+
+export default recipies
