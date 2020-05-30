8a0f21b81a452d6711f731165d02598647419e56
Gr8z <Gr8z@users.noreply.github.com>
2020-05-30 13:02:30 +0000

    deploy: de88b7b131fc151dd27b3cf8f7c994357fe44b14

---
+++ precache-manifest.12b8de0b5ebec152e319819ada3b0ef0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af2e844cbc7f13d40cbca18c0188d24",
+    "url": "/cookbook-test/index.html"
+  },
+  {
+    "revision": "9087d665516d743a4a5c",
+    "url": "/cookbook-test/static/js/2.ce842130.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cookbook-test/static/js/2.ce842130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a103a70541df02146f88",
+    "url": "/cookbook-test/static/js/main.de594451.chunk.js"
+  },
+  {
+    "revision": "38c7785058bbcf747868",
+    "url": "/cookbook-test/static/js/runtime-main.3cb9b9c2.js"
+  }
+]);
