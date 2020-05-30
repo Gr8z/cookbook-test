0d3d56b910119e464d49b21156bccf0b9ace5ce0
GR8 <gr8_boi52@yahoo.com>
2020-05-30 17:10:35 +0400

    Check for bad handles

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -21,9 +21,11 @@ const Recipe = () => {
 
   // Set the favorite status
   useEffect(() => {
-    const isFav = favHandles && favHandles.includes(recipe.handle)
-    setFav(isFav)
-  }, [favHandles, recipe.handle])
+    if (index > 0) {
+      const isFav = favHandles && favHandles.includes(recipe.handle)
+      setFav(isFav)
+    }
+  }, [favHandles, recipe, index])
 
   // toggle favourite (create the array if it doesnt exist)
   const handleClick = () => {
@@ -48,32 +50,36 @@ const Recipe = () => {
 
   return (
     <RecipeDetail>
-      <RecipeDetail.Title>
-        {recipe.name}{' '}
-        <RecipeDetail.Fav onClick={() => handleClick()}>
-          {fav ? '💔' : '💖'}
-        </RecipeDetail.Fav>
-      </RecipeDetail.Title>
-      <RecipeDetail.Meta>
-        <RecipeDetail.Thumb src={recipe.img} alt={recipe.handle} />
-        <p>Author: {recipe.author}</p>
-        <p>Prop Time: {recipe.times.prep} mins</p>
-        <p>Cook Time: {recipe.times.cook} mins</p>
-      </RecipeDetail.Meta>
-      <RecipeDetail.Heading>Description</RecipeDetail.Heading>
-      <p>{recipe.description}</p>
-      <RecipeDetail.Heading>Ingredients</RecipeDetail.Heading>
-      <RecipeDetail.Ingredients>
-        {recipe.ingredients.map((item, i) => (
-          <li key={i}>{item}</li>
-        ))}
-      </RecipeDetail.Ingredients>
-      <RecipeDetail.Heading>Instructions</RecipeDetail.Heading>
-      <RecipeDetail.instructions>
-        {recipe.instructions.map((item, i) => (
-          <li key={i}>{item}</li>
-        ))}
-      </RecipeDetail.instructions>
+      {index > 0 && (
+        <>
+          <RecipeDetail.Title>
+            {recipe.name}{' '}
+            <RecipeDetail.Fav onClick={() => handleClick()}>
+              {fav ? '💔' : '💖'}
+            </RecipeDetail.Fav>
+          </RecipeDetail.Title>
+          <RecipeDetail.Meta>
+            <RecipeDetail.Thumb src={recipe.img} alt={recipe.handle} />
+            <p>Author: {recipe.author}</p>
+            <p>Prop Time: {recipe.times.prep} mins</p>
+            <p>Cook Time: {recipe.times.cook} mins</p>
+          </RecipeDetail.Meta>
+          <RecipeDetail.Heading>Description</RecipeDetail.Heading>
+          <p>{recipe.description}</p>
+          <RecipeDetail.Heading>Ingredients</RecipeDetail.Heading>
+          <RecipeDetail.Ingredients>
+            {recipe.ingredients.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </RecipeDetail.Ingredients>
+          <RecipeDetail.Heading>Instructions</RecipeDetail.Heading>
+          <RecipeDetail.instructions>
+            {recipe.instructions.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </RecipeDetail.instructions>
+        </>
+      )}
     </RecipeDetail>
   )
 }
